ad7117782d5ad951e8e5514d4d4d2f01eadaeb07
Timur Baklanov <timbaklanov03@gmail.com>
2021-10-22 18:16:21 +0300

    Initial Commit

---
+++ assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment2
+//
+//  Created by Тимур Бакланов on 22.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
